e00ce5b16fabfa8338f3815d11370a0a82d295f9
Danezis <kdanezis@gmail.com>
2020-02-18 22:22:51 +0100

    update default var

--- script/User.class.js
+++ script/User.class.js
@@ -7,16 +7,18 @@ class User {
 
   reset(){
     this.taxeA = 20;
-    this.taxeB = 15;
+    this.taxeB = 5;
     this.taxeC = 35;
     this.isDead = false;
-    this.rank = "Duc";
+    this.rank = "Baron";
     this.country = "France";
-    this.harvest = 10;
-    this.supply = 100;
-    this.needPeople = 1000;
-    this.needOst = 1000;
-    this.money = 50;
+    this.harvest = 17524;
+    this.supply = 12842;
+    this.people = 2026;
+    this.needPeople = 10140;
+    this.ost=20;
+    this.needOst = 160;
+    this.money = 1000;
     this.land = 5000;
   }
 
